8c410a70f2cac7a468a32eb46b0d936ba5e4a296
3bd-allah <abdullah.nabil5044@gmail.com>
2024-05-21 00:08:05 +0300

    Add Report progress and COmbinators

---
+++ Combinators/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Combinators
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var has1000Subscribers = Task.Run(Has1000Subscribers);
+            var has4000ViewHours = Task.Run(Has4000ViewHours);
+
+            Console.WriteLine("Using WhenAny()");
+            Console.WriteLine("----------------");
+            var any = await Task.WhenAny(has1000Subscribers, has4000ViewHours);
+            Console.WriteLine(any.Result);
+
+            Console.WriteLine("Using WhenAll()");
+            Console.WriteLine("----------------");
+            var all = await Task.WhenAll(has1000Subscribers, has4000ViewHours);
+            foreach(var t in all)
+                Console.WriteLine($"{t}");
+
+            Console.ReadKey(true);
+
+        }
+
+        public static async Task<string> Has1000Subscribers()
+        {
+            await Task.Delay(4000);
+            return "Congrats !! you have reached 1000 subscribers";
+        }
+
+        public static async Task<string> Has4000ViewHours()
+        {
+            await Task.Delay(3000);
+            return "Congrats !! you have reached 4000 view hours";
+        }
+    }
+}
---
+++ ReportProgressFromAsyncFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ReportProgressFromAsyncFunction
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Action<int> progress = (n) =>
+            {
+                Console.Clear();
+                Console.WriteLine($"{n}%");
+            };
+
+            await Copy(progress);
+
+            Console.ReadKey();
+        }
+
+        public static Task Copy(Action<int> OnProgress)
+        {
+            return Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Task.Delay(50).Wait();
+                    if (i % 10 == 0)
+                    {
+                        OnProgress(i);
+                    }
+                }
+            });
+        }
+    }
+}
